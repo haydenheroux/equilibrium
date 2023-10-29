921598639ebe6292c63c0bc5756622e164ae6d0e
Hayden Heroux <haydenheroux@gmail.com>
2023-10-29 13:14:21 -0400

    feat: Improve user interface.

--- main.go
+++ main.go
@@ -11,22 +11,29 @@ func main() {
 
 	lt := stats.LeastSquares(points)
 
-	var waterTemp, waterMass, iceTemp, iceMass float64
+	var initialMass, waterTemp, postWaterMass, iceTemp, postIceMass float64
 
+	fmt.Print("initial mass: ")
+	fmt.Scanf("%f", &initialMass)
 	fmt.Print("water temp: ")
 	fmt.Scanf("%f", &waterTemp)
-	fmt.Print("water mass: ")
-	fmt.Scanf("%f", &waterMass)
+	fmt.Print("post-water mass: ")
+	fmt.Scanf("%f", &postWaterMass)
 	fmt.Print("ice temp: ")
 	fmt.Scanf("%f", &iceTemp)
-	fmt.Print("ice mass: ")
-	fmt.Scanf("%f", &iceMass)
+	fmt.Print("post-ice mass: ")
+	fmt.Scanf("%f", &postIceMass)
+
+	waterMass := postWaterMass - initialMass
+	iceMass := postIceMass - postWaterMass
 
 	x := thermodynamics.EquilibriumTemperature(waterMass, waterTemp, iceMass, iceTemp)
 
 	estimatedTemp := lt.Apply(x)
 
 	fmt.Printf("estimated temp: %.4f\n", estimatedTemp)
+
+	fmt.Printf("\nThe equilibrium temperature will be %.2f degress Celsius.\n\n", estimatedTemp)
 
 	var actualTemp float64
 
@@ -35,5 +42,12 @@ func main() {
 
 	error := stats.ApproximationError(estimatedTemp, actualTemp)
 
-	fmt.Printf("percent error: %.4f\n", error*100)
+	fmt.Printf("error: %.4f\n", error)
+
+	percentError := error * 100
+	if percentError < 0 {
+		fmt.Printf("\nThe actual value is %.1f%% below the estimated value.\n\n", -1*percentError)
+	} else {
+		fmt.Printf("\nThe actual value is %.1f%% above the estimated value.\n\n", percentError)
+	}
 }
